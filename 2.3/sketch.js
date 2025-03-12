40ce1598ca483f56126cc7b5bcce5e24e2a8ba9b
Kyler VanCamp <105409263+Kyler115@users.noreply.github.com>
2025-03-11 21:45:19 -0500

    Add files via upload

---
+++ 2.3/sketch.js
@@ -0,0 +1,108 @@
+let basicSynth, filt, LFOfilt, panner, fmSynth, values, noise1, noiseEnv, filt1, values1,imageFlag;
+
+function preload() {
+  img = loadImage("media/cannonball.jpeg");
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  panner = new Tone.AutoPanner({
+    frequency: 0.1,
+    depth: 1
+  }).toDestination().start()
+
+  filt = new Tone.Filter(300, "lowpass", -48).connect(panner);
+
+  basicSynth = new Tone.Synth().connect(filt);
+
+  LFOfilt = new Tone.LFO(0.1, 200, 2000).start();
+
+  fmSynth = new Tone.FMSynth({
+    harmonicity: 1,
+    modulationIndex: 10
+  }).toDestination();
+
+  values = new Float32Array([1, 0.02, 0.3, 15, 15, 0.3, 1]);
+
+  filt1 = new Tone.AutoFilter({
+    frequency: 0.1,
+    depth: 0.3,
+    baseFrequency: 500,
+    octaves: 4
+  }).toDestination().start();
+
+  noiseEnv = new Tone.AmplitudeEnvelope({
+    attack: 3,
+    decay: 0.1,
+    sustain: 1,
+    release: 1
+  }).connect(filt1)
+
+  noise1 = new Tone.Noise().connect(noiseEnv).start();
+  noiseFilt = new Tone.LFO(0.1, 0, 1).start();
+  noiseFilt.connect(filt1.frequency);
+
+  values1 = new Float32Array([-96, -30, -30, -12, 0, -12, 0, 0, -6, -12, -30, -96])
+}
+
+function draw() 
+{
+  background(220);
+  text("Click for cannonball!",100,100);
+  if(imageFlag)
+  {
+    image(img,0,0,400,400);
+  }
+}
+
+function mouseClicked() 
+{
+  if (mouseX<=400 && mouseY<=400)
+  {
+    imageFlag = true;
+    noiseEnv.triggerAttackRelease(1);
+  }
+}
+
+// let synth1, filt, rev;
+
+// function setup() {
+//   createCanvas(300, 300);
+
+//   img = loadImage("cannonball.jpeg");
+
+  
+//   filt = new Tone.Filter(1000, "lowpass").toDestination();
+//   rev = new Tone.Reverb(2).connect(filt);
+//   rev.wet.value = 0;
+
+//   synth1 = new Tone.Synth({
+//     envelope: {
+//       attack: 0,
+//       decay: 0,
+//       sustain: 1,
+//       release: 1
+//     }
+//   }).connect(rev);
+// }
+
+// function draw() 
+// {
+//   background(220);
+
+//   text("Click for cannonball!",100,100);
+
+// }
+
+// function mouseClicked() 
+// {
+//   if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) 
+//   {
+//     Tone.start(); 
+
+//     synth1.triggerAttackRelease("C4", "1n");
+//     noiseEnv.triggerAttackRelease(0.5);
+//     image(img,300,300);
+//   }
+// }
